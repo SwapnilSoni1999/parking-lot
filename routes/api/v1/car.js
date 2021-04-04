f6cbaea4ae5455c3ac4b691619bdbab5719cdd0a
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-05 04:33:15 +0530

    routes: car: register moveTo route

--- routes/api/v1/car.js
+++ routes/api/v1/car.js
@@ -8,5 +8,6 @@ const router = Router()
 router.route('/park').post(catchAsync(Car.park))
 router.route('/unpark/:slotId').delete(catchAsync(Car.unpark))
 router.route('/get-info').get(catchAsync(Car.getInfo))
+router.route('/move').post(catchAsync(Car.moveTo))
 
 module.exports = router
