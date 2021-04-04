8d700244ecad4ef8b4dd60a6b07c48ff784e84ca
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-04 17:24:13 +0530

    app: init basic server

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const express = require('express')
 const ParkingLot = require('./lib/ParkingLot')
 
 const parkingLot = new ParkingLot(process.env.PARKING_LOT_SIZE)
+
+const app = express()
+const PORT = 3000
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
