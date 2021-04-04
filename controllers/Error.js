23e0a822f386826ebdf899a12ae6fac2abb3d81d
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-04 18:58:59 +0530

    controllers: error: register NotFoundError

--- controllers/Error.js
+++ controllers/Error.js
@@ -1,4 +1,4 @@
-const { ValidationError, NoParkingSlot } = require("../lib/error")
+const { ValidationError, NoParkingSlot, NotFoundError } = require("../lib/error")
 
 const errorController = (err, _, res, _next) => {
     if (err instanceof ValidationError) {
@@ -7,6 +7,10 @@ const errorController = (err, _, res, _next) => {
 
     if (err instanceof NoParkingSlot) {
         return res.status(409).json({ message: err.message, type: err.name })
+    }
+
+    if (err instanceof NotFoundError) {
+        return res.status(404).json({ message: err.message, type: err.name })
     }
 
     return res.status(500).json({ message: "Internal server error!" })
