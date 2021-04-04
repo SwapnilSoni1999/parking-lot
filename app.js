be9cb9796150b4ca9a16c58fd621f04af6e9f59d
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-04 18:08:32 +0530

    app: register route to express server

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const config = require('./config')
 const express = require('express')
 
+const routes = require('./routes')
+const errorController = require('./controllers/Error')
+
 const app = express()
 const PORT = 3000
+
+app.use('/api', routes)
+
+app.use(errorController)
 
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
