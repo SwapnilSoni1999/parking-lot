7c7af41052e5c1acabfa23a864e335b5f2fd1f84
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-04 18:17:56 +0530

    controllers: car: add unpark handler

--- controllers/Car.js
+++ controllers/Car.js
@@ -6,11 +6,17 @@ class Car {
      * @param {Express.Request} req 
      * @param {Express.Response} res
      */
-    static async park(req, res) {
+    static async park(req, res, next) {
         // ref: carName
         const { car_number } = req.body
         const parkedData = parkingLot.park(car_number)
         return res.json({ message: "Parked car!", ...parkedData })
+    }
+
+    static async unpark(req, res, next) {
+        const { slotId } = req.params
+        parkingLot.unpark(slotId)
+        return res.json({ message: "Unparked car!" })
     }
 }
 
