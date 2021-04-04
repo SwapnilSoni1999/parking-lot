696b5a01e03007a0f65112797d008bc3bb386eb6
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-04 18:18:34 +0530

    [v1] routes: car: register unpark controller

--- routes/api/v1/car.js
+++ routes/api/v1/car.js
@@ -6,5 +6,6 @@ const catchAsync = require('../../../utils/catchAsync')
 const router = Router()
 
 router.route('/park').post(catchAsync(Car.park))
+router.route('/unpark').delete(catchAsync(Car.unpark))
 
 module.exports = router
