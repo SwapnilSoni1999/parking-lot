a9d4a963366499da8daff9b8c996649c5939f1e3
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-04 18:05:17 +0530

    controllers: error: init error controller

---
+++ controllers/Error.js
@@ -0,0 +1,15 @@
+const { ValidationError, NoParkingSlot } = require("../lib/error")
+
+const errorController = (err, _, res, _next) => {
+    if (err instanceof ValidationError) {
+        return res.status(422).json({ message: err.message, type: err.name })
+    }
+
+    if (err instanceof NoParkingSlot) {
+        return res.status(409).json({ message: err.message, type: err.name })
+    }
+
+    return res.status(500).json({ message: "Internal server error!" })
+}
+
+module.exports = errorController
