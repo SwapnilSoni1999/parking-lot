fa4126e8860fbc865f1caae1687a7c6b5d15f3eb
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-04 17:31:21 +0530

    app: remove unnecessary imports

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const config = require('./config')
 const express = require('express')
 
-const ParkingLot = require('./lib/ParkingLot')
-
-const parkingLot = new ParkingLot(process.env.PARKING_LOT_SIZE)
-
 const app = express()
 const PORT = 3000
 
