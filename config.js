d585dfc2af0fe5a9d6f3d74f11083228bc74a841
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-04 17:20:08 +0530

    config: init with dotenv and setup criteria

---
+++ config.js
@@ -0,0 +1,11 @@
+require('dotenv')
+
+if (!process.env.PARKING_LOT_SIZE) {
+    throw new Error(`PARKING_LOT_SIZE is not defined! Please define from .env`)
+}
+
+if (!process.env.PARKING_LOT_SIZE < 0) {
+    throw new Error(`PARKING_LOT_SIZE must be greater than 0.`)
+}
+
+console.info('Loaded config!')
