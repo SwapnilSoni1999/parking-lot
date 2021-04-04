a51d21f77b987809af838dfae3d519f753180a12
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-04-04 18:57:26 +0530

    routes: car: register get-info route

--- routes/api/v1/car.js
+++ routes/api/v1/car.js
@@ -7,5 +7,6 @@ const router = Router()
 
 router.route('/park').post(catchAsync(Car.park))
 router.route('/unpark').delete(catchAsync(Car.unpark))
+router.route('/get-info').get(catchAsync(Car.getInfo))
 
 module.exports = router
